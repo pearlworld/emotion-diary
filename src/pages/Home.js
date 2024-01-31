4b9f9e6e6d64f128b2da77f8d00e5fe9622961e4
pearl <ddch4747@naver.com>
2024-01-31 16:17:27 +0900

    240131 프로젝트 빌드&테스트

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { DiaryStateContext } from '../App';
 import MyHeader from '../components/MyHeader';
 import MyButton from '../components/MyButton';
-import { DiaryStateContext } from '../App';
 import DiaryList from '../components/DiaryList';
 
 const Home = () => {
@@ -11,6 +11,11 @@ const Home = () => {
   const [curDate, setCurDate] = useState(new Date());
 
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장`;
+  }, []);
 
   useEffect(() => {
     if (diaryList.length >= 1) {
